a70de3bf7cff245b4c339e6927a48418f90c3841
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-21 23:12:51 +0200

    refactor comments functionality; implement postComment method for adding new comments; enhance comment form with validation and error handling

--- app/Livewire/Comments/Comments.php
+++ app/Livewire/Comments/Comments.php
@@ -7,10 +7,29 @@ use Livewire\Component;
 
 class Comments extends Component {
     public ?Model $model = null;
+
+    /** @var array{body:string} */
+    public array $newCommentState = ['body' => ''];
+
     public function render() {
         $comments = $this->model?->comments()->onlyParent()->latest()->get();
         return view('livewire.comments.comments', [
             'comments' => $comments ?? [],
         ]);
+    }
+
+    public function postComment() {
+        $this->validate([
+            'newCommentState.body' => ['required', 'min:3', 'max:255'],
+        ], [
+            'newCommentState.body.required' => 'You cant leave the comment empty',
+            'newCommentState.body.min' => 'The comment cant be less than 3 chars',
+            'newCommentState.body.max' => 'The comment body cant be more than 255 chars',
+        ]);
+        $this->model->comments()->create([
+            'user_id' => auth()->id(),
+            'body' => $this->newCommentState['body'],
+        ]);
+        $this->newCommentState['body'] = '';
     }
 }
--- resources/views/livewire/comments/comments.blade.php
+++ resources/views/livewire/comments/comments.blade.php
@@ -15,12 +15,20 @@
     @endforelse
     @auth
     <div class="p-2  bg-gray-50 pt-5 space-y-4">
-        <div class="flex space-x-2 items-start">
-            <img src="{{ auth()->user()->avatarUrl() }}" alt="" class="w-10 h-10 rounded-full">
-            <textarea placeholder="Leave A Comment" class="w-full rounded-xl "></textarea>
-        </div>
-        <x-primary-button>
-            Comment</x-primary-button>
+        <form wire:submit.prevent="postComment">
+
+            <div class="flex space-x-2 items-start">
+                <img src="{{ auth()->user()->avatarUrl() }}" alt="" class="w-10 h-10 rounded-full">
+                <div class="flex flex-col w-full gap-y-2">
+                    <textarea wire:model="newCommentState.body" placeholder="Leave A Comment" class="w-full rounded-xl @error('newCommentState.body') border-red-500 @enderror "></textarea>
+                    @error('newCommentState.body')
+                    <div class="text-xs text-red-500 px-3">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+            <x-primary-button class="mt-4">
+                Comment</x-primary-button>
+        </form>
     </div>
     @endauth
     @guest
